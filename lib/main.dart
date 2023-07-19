8862b884065ddaf575fd38b5c08e561c2b2378ef
arorajasman <arorajasman97@gmail.com>
2023-07-19 07:50:34 +0530

    added values manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:e_commerce_app/app/app.dart';
 import 'package:flutter/material.dart';
 
+// NOTE: There should be no hardcoded values inside the app
+
 void main() {
   runApp(MyApp());
 }
---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,55 @@
+import 'package:e_commerce_app/presentation/font_manager.dart';
+import 'package:flutter/material.dart';
+
+TextStyle _getTextStyle(
+  double fontSize,
+  String fontFamily,
+  FontWeight fontWeight,
+  Color color,
+) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontFamily: fontFamily,
+    color: color,
+    fontWeight: fontWeight,
+  );
+}
+
+// getting regular font style
+TextStyle getRegularFontStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManger.regular,
+    color,
+  );
+}
+
+// getting light text style
+TextStyle getLightFontStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManger.light,
+    color,
+  );
+}
+
+// getting bold text style
+TextStyle getBOldFontStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+    fontSize,
+    FontConstants.fontFamily,
+    FontWeightManger.bold,
+    color,
+  );
+}
